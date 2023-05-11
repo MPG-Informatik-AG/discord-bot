bb9fe3f5909f84cf4873aaf67f3e96d14e4c9e0c
Marc Binder <bindermarc@gmx.de>
2023-05-11 13:37:03 +0200

    added canvas library and started generating tt images

--- src/commands/getTT.ts
+++ src/commands/getTT.ts
@@ -1,6 +1,8 @@
-import { CommandInteraction, ApplicationCommandType, Client } from "discord.js"
+import { CommandInteraction, ApplicationCommandType, Client, AttachmentBuilder } from "discord.js"
 import { Command } from "../command"
 import { getTimetable } from "../webuntis/apiClient"
+import * as Canvas from "@napi-rs/canvas"
+import { Lesson } from "webuntis"
 
 export const getTT: Command = {
     name: "gettt",
@@ -8,7 +10,7 @@ export const getTT: Command = {
     type: ApplicationCommandType.ChatInput,
     run: async (client: Client, interaction: CommandInteraction) => {
 
-        const times = [740, 830, 935, 1020, 1115, 1205, 1345, 1435, 1525, 1610] 
+        const times = [740, 830, 935, 1020, 1115, 1205, 1255, 1345, 1435, 1525, 1610] 
         let tt = await getTimetable()
 
         const content = times.map(time => {
@@ -22,7 +24,31 @@ export const getTT: Command = {
 
         await interaction.followUp({
             ephemeral: true,
-            content: content
+            content: content,
+            files: [await generateImage(times, tt)]
         })
     }
+}
+
+async function generateImage (times: number[], tt: {[key: number]: Lesson[];}) {
+    const pixel_per_class = 100;
+    const canvas = Canvas.createCanvas(700, times.length * pixel_per_class); 
+    const context = canvas.getContext('2d');
+
+
+    // const background = await Canvas.loadImage('./image.png');
+    //
+    // context.drawImage(background, 0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < times.length; i++) {
+        context.fillStyle = "grey"
+        context.fillRect(0, i*pixel_per_class, 700, pixel_per_class-3)
+    
+        context.fillStyle = "black"
+        context.font = "15px serif"
+        if (tt[times[i]] != undefined) {
+            context.fillText(tt[times[i]]?.[0].sg || "some class", 10, i*pixel_per_class + 10)
+        }
+    }
+
+    return new AttachmentBuilder(await canvas.encode('webp'), { name: 'profile-image.png' });
 }
--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,15 +1,27 @@
-import { CommandInteraction, ApplicationCommandType, Client } from "discord.js"
+import { CommandInteraction, ApplicationCommandType, Client, AttachmentBuilder } from "discord.js"
 import { Command } from "../command"
+import * as Canvas from "@napi-rs/canvas"
 
 export const Hello: Command = {
     name: "hello",
     description: "Says hello",
     type: ApplicationCommandType.ChatInput,
     run: async (client: Client, interaction: CommandInteraction) => {
+
+		const canvas = Canvas.createCanvas(700, 250); 
+		const context = canvas.getContext('2d');
+
+        const background = await Canvas.loadImage('./image.png');
+
+        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+        const attachment = new AttachmentBuilder(await canvas.encode('webp'), { name: 'profile-image.png' });
+
         const content = "Hi there"
         await interaction.followUp({
             ephemeral: true,
-            content: content
+            content: content,
+            files: [attachment],
         })
     }
 }
